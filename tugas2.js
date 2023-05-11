28a0e0d45b9d94ae65ee023ddb5f403c547d7213
AirPutih19 <kharismadwisj@gmail.com>
2023-05-11 16:06:49 +0700

    Tugas 2 JS
    Tugas 2 JS untuk niomic

---
+++ tugas2.js
@@ -0,0 +1,19 @@
+var angka = 19;
+var huruf = "Kacang";
+var penjualan = true;
+var biji = 210;
+if(biji>200)
+var penjualan= true;
+else 
+var penjualan = false;
+var laku= "12 perhari";
+laku=null;
+var dibuang;
+var tanggal = [15,16,17,18,19]
+
+console.log(angka)
+console.log(huruf)
+console.log(penjualan)
+console.log(laku);
+console.log(dibuang);
+console.log(tanggal[3])
